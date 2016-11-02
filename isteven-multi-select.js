a0572434d3623a4455b8869fb174f619d67dacf3
Langdon Oliver <langdonx@protossecurity.com>
2016-11-02 15:28:07 -0400

    remove background from multi-select, lighten border, use "form-control" class for bs shadow

--- isteven-multi-select.js
+++ isteven-multi-select.js
@@ -1040,7 +1040,9 @@ angular.module( 'isteven-multi-select', ['ng'] ).directive( 'istevenMultiSelect'
     var template = 
         '<span class="multiSelect inlineBlock">' +
             // main button
-            '<button id="{{directiveId}}" type="button"' +                
+            '<button id="{{directiveId}}"' +
+                'class="form-control"' +
+                'type="button"' +
                 'ng-click="toggleCheckboxes( $event ); refreshSelectedItems(); refreshButton(); prepareGrouping; prepareIndex();"' +
                 'ng-bind-html="varButtonLabel"' +
                 'ng-disabled="disable-button"' +
